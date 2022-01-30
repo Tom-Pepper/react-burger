41c92306356f248803404a3a2a5a72a73f85b368
Artem Kondratyev <walinor.88@gmail.com>
2022-01-30 17:48:07 +0300

    feat: add ing modal and styles

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -8,7 +8,30 @@ const IngredientDetails = ({ isOpen, onClose, title }) => {
   return(
     <>
       <Modal isOpen={isOpen} onClose={onClose} title={title}>
-
+        <div className={ingDetailsStyle.info_wrapper}>
+          <img src="" alt=""/>
+          <h4 className={ingDetailsStyle.details__item_name}>
+            Биокотлета
+          </h4>
+          <ul className={ingDetailsStyle.items__list}>
+            <li className={ingDetailsStyle.items__item}>
+              <p className={ingDetailsStyle.element_text}>Калории</p>
+              <p className={ingDetailsStyle.element_quantity}>111</p>
+            </li>
+            <li className={ingDetailsStyle.items__item}>
+              <p className={ingDetailsStyle.element_text}>Белки</p>
+              <p className={ingDetailsStyle.element_quantity}>111</p>
+            </li>
+            <li className={ingDetailsStyle.items__item}>
+              <p className={ingDetailsStyle.element_text}>Жиры</p>
+              <p className={ingDetailsStyle.element_quantity}>111</p>
+            </li>
+            <li className={ingDetailsStyle.items__item}>
+              <p className={ingDetailsStyle.element_text}>Углеводы</p>
+              <p className={ingDetailsStyle.element_quantity}>111</p>
+            </li>
+          </ul>
+        </div>
       </Modal>
     </>
   );
