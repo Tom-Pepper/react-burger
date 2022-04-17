b2d88c74531551b8e91e5038847ab07402396615
Artem Kondratyev <walinor.88@gmail.com>
2022-04-17 16:48:18 +0300

    refactor: render menu with Redux

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -4,26 +4,27 @@
  * and ingredients between them
  */
 
-import React, {useContext, useEffect, useState} from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 import burgerConstructor from './burger-constructor.module.css';
 import {Button, ConstructorElement, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import ConstructorCard from "../constructor-card/constructor-card";
-import {MenuContext} from "../../services/menuContext";
+import {useSelector} from "react-redux";
 
 const BurgerConstructor = ({ onSubmit }) => {
-  const ingredients = useContext(MenuContext);
+  const { constructorIngs } = useSelector(state => state.ingredients);
+  console.log(constructorIngs)
 
   const orderIDs = [];
   const withoutBuns = [];
 
-  ingredients.forEach(item => {
+  constructorIngs.forEach(item => {
     if (item.type !== 'bun') {
       withoutBuns.push(item);
     }
   });
 
-  const bun = ingredients.find((item) => item.type === 'bun');
+  const bun = constructorIngs.find((item) => item.type === 'bun');
 
   //Renders buns on top and to the bottom
   function renderBun (position, positionName) {
@@ -46,11 +47,14 @@ const BurgerConstructor = ({ onSubmit }) => {
 
   //Total sum
   const calculateBurger = React.useMemo(() => {
-    const bunsTotal = bun.price * 2;
-    const ingsTotal = withoutBuns.reduce(function (sum, current) {
-      return sum + current.price;
-    }, 0);
-    return bunsTotal + ingsTotal;
+    if (bun) {
+      const bunsTotal = bun.price * 2;
+      const ingsTotal = withoutBuns.reduce(function (sum, current) {
+        return sum + current.price;
+      }, 0);
+      return bunsTotal + ingsTotal;
+    }
+    return 0;
   }, [bun, withoutBuns])
 
   function handlePlaceOrder (e) {
@@ -67,21 +71,26 @@ const BurgerConstructor = ({ onSubmit }) => {
   return (
     <section className={burgerConstructor.wrapper}>
       <ul className={burgerConstructor.constructor__list}>
-        <li className={burgerConstructor.lockedItem}>
-          {renderBun('top', '(Верх)')}
-        </li>
-        <div className={burgerConstructor.inner__list}>
-          {renderIngs()}
-        </div>
-        <li className={burgerConstructor.lockedItem}>
-          {renderBun('bottom', '(Низ)')}
-        </li>
+        {
+          constructorIngs.length > 0 &&
+          <>
+            <li className={burgerConstructor.lockedItem}>
+              {renderBun('top', '(Верх)')}
+            </li>
+            <div className={burgerConstructor.inner__list}>
+              {renderIngs()}
+            </div>
+            <li className={burgerConstructor.lockedItem}>
+              {renderBun('bottom', '(Низ)')}
+            </li>
+          </>
+        }
       </ul>
 
       <form className={burgerConstructor.constructor__form}>
         <div className={burgerConstructor.summ__wrapper}>
           <p className={burgerConstructor.summ__price}>
-            {calculateBurger}
+            { constructorIngs.length > 0 ? calculateBurger : 0 }
           </p>
           <CurrencyIcon type={"primary"} />
         </div>
